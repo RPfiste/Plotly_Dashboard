8661aeb1a39a30ad1c7413a0709ce682a84929f0
Ramon_Pfister <ramon.pfistr001@gmail.com>
2022-12-30 16:53:12 +0100

    histogram addet

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.read_excel(r"C:\Users\ramon\Git\Plotly_Dashboard\data\KANTON_ZUERICH_Primaersektor.xls")
+print(df.head())
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+df_gemeinde = df[df['GEBIET_NAME'].str.contains('Region') == False]
+df_gemeinde = df_gemeinde[df_gemeinde['GEBIET_NAME'].str.contains('ganzer Kanton') == False]
+
+fig = px.histogram(df_gemeinde, x="INDIKATOR_VALUE" ,nbins=100)
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
