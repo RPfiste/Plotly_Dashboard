308e3d906a263c0468249924b78b9aae5d32564d
Ramon_Pfister <ramon.pfistr001@gmail.com>
2023-01-09 22:17:10 +0100

    dropdown_ortsname

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ app = Dash(__name__)
 
 #Definieren der Farbcodes
 colors = {
-    'background' : '#000000',
-    'text' : '#7FDBFF'
+    'background' : '#838b8b',
+    'text' : '#FFFFFF',
+    'titel' : '#ffa500',
+    'balken' : '#ffa500'
 }
 
 # assume you have a "long-form" data frame
@@ -19,53 +21,54 @@ df = pd.read_excel(r"C:\Users\ramon\Git\Plotly_Dashboard\data\KANTON_ZUERICH_Pri
 print(df.head())
 
 #entferne der Zeilen, die "Region" oder "ganzer Kanton" beinhalten
-df_gemeinde = df[df['GEBIET_NAME'].str.contains('Region') == False]                             #definiert als df_gemeinde alle daten die nicht "Region" beinhalten
-df_gemeinde = df_gemeinde[df_gemeinde['GEBIET_NAME'].str.contains('ganzer Kanton') == False]    #definiert als df_gemeinde alle daten die nicht "ganzer Kanton" beinhalten
-df_gemeinde = df_gemeinde[df_gemeinde['GEBIET_NAME'].str.contains('Bezirk') == False]           #definiert als df_gemeinde alle daten die nicht "Bezirk" beinhalten
-
-
-#nach GEBIETS_NAME gruppieren
-df_gemeinde= df_gemeinde.groupby('GEBIET_NAME').mean()
+df_kanton_bezirk = df[df['GEBIET_NAME'].str.contains('Region') == False]                             #definiert als df_gemeinde alle daten die nicht "Region" beinhalten
+df_bezirk = df_kanton_bezirk[df_kanton_bezirk['GEBIET_NAME'].str.contains('ganzer Kanton') == False]    #definiert als df_gemeinde alle daten die nicht "ganzer Kanton" beinhalten
+df_gemeinde = df_bezirk[df_bezirk['GEBIET_NAME'].str.contains('Bezirk') == False]           #definiert als df_gemeinde alle daten die nicht "Bezirk" beinhalten
 print(df_gemeinde.head())
 
+#nach GEBIETS_NAME gruppieren
+df_gemeinde_mean= df_gemeinde.groupby('GEBIET_NAME').mean()
+print(df_gemeinde_mean.head())
+
 #Plot erstellen
-fig = px.bar(df_gemeinde, y='INDIKATOR_VALUE', barmode='group')
+plot_gemeinde_mean = px.bar(df_gemeinde_mean, y='INDIKATOR_VALUE', barmode='group')
+
 
 #Layout anpassen
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
+plot_gemeinde_mean.update_layout(
+    plot_bgcolor=colors['background'],      #Hintergrundfareb hinter den Balken
+    paper_bgcolor=colors['background'],     #Hintergrundfarbe hinter Text
+    font_color=colors['text'],
 )
-
-#Spalteninhalt zuweisen
 
 
 
 #Erstellen der Beschriftung des Dashboards
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[       #Hintergrundfarbe bestimmen
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[       #Hintergrundfarbe hinter Titel bestimmen
     html.H1(
         children='Dashboard',                                                           #Titel: Dashboard
         style={
             'textAlign': 'center',                                                      #Zentriert darstellen
-            'color': colors['text']                                                     #Textfarbe bestimmen
+            'color': colors['titel']                                                     #Textfarbe bestimmen
     }),
     html.Div(children='Durchschnittliche Anzahl Arbeitsstätten im Primärsektor nach Gemeinden.',
         style={                                                                         #Plotbeschritung erstellen
             'textAlign': 'center',                                                          #Beschriftung zentrieren
             'color': colors['text']                                                         #Textfarbe bestimmen
     }),
-    html.Div(children=[
-            html.Label('Dropdown'),
-            dcc.Dropdown(['GEBIET_NAME']),
+    html.Div(children='Wählen sie eine Gemeinde',
+        style={
+            'color' : colors['text']
+        }),
+            dcc.Dropdown(df_gemeinde['GEBIET_NAME']),
 
 
 
 
-        ], ),
+
     dcc.Graph(
         id='example-graph',
-        figure=fig
+        figure=plot_gemeinde_mean
     )
 ])
 
